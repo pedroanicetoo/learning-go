fac32e06de4bc3931f834a89ee218ef99c0e5c7f
pedroanicetoo <pedro0077@gmail.com>
2023-04-08 01:16:14 -0300

    arrays and slices

---
+++ 9 - Arrays and Slices/arrays_and_slices.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	fmt.Println("Arrays and Slices")
+
+	var array1 [5]string
+	array1[0] = "index 0"
+	fmt.Println(array1) // [index 0    ]
+
+	array2 := [5]string{"index0", "index1", "index2", "index3", "index4"}
+	fmt.Println(array2) // [index0 index1 index2 index3 index4]
+
+	array3 := [...]int{1, 2, 3, 4, 5}
+	fmt.Println(array3) // [1 2 3 4 5]
+
+	slice := []int{10, 11, 12, 13, 14, 15, 16, 17}
+	fmt.Println(slice) // [10 11 12 13 14 15 16 17]
+
+	fmt.Println(reflect.TypeOf(slice))  // []int
+	fmt.Println(reflect.TypeOf(array3)) // [5]int
+
+	slice = append(slice, 18)
+	fmt.Println(slice)
+
+	slice2 := array2[1:3]
+	fmt.Println(slice2) // [index1 index2]
+
+	array2[1] = "Index1Change"
+	fmt.Println(slice2) // [Index1Change index2]
+}
