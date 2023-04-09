c01dd8a953e2312b96f413b90706b3b59ac8d0c8
pedroanicetoo <pedro0077@gmail.com>
2023-04-09 01:10:21 -0300

    create functionality of Server search

--- 17 - Command Line Application/app/app.go
+++ 17 - Command Line Application/app/app.go
@@ -14,17 +14,25 @@ func Generate() *cli.App {
 	app.Name = "Command Line Application"
 	app.Usage = "Search IP'S and Server Names on Internet"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "devbook.com.br",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Search IP's on the internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "devbook.com.br",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Search IP's on the internet",
+			Flags:  flags,
 			Action: searchIPs,
+		},
+		{
+			Name:   "servers",
+			Usage:  "Search server name on the Internet",
+			Flags:  flags,
+			Action: searchServers,
 		},
 	}
 	return app
@@ -41,5 +49,19 @@ func searchIPs(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func searchServers(c *cli.Context) {
+	host := c.String("host")
+
+	servers, erro_r := net.LookupNS(host) // name server
+
+	if erro_r != nil {
+		log.Fatal(erro_r)
+	}
+
+	for _, server := range servers {
+		fmt.Println(server.Host)
 	}
 }
