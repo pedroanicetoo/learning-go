205880cd7b5ad15f4ba1f33532d75ace5c00ed36
pedroanicetoo <pedro0077@gmail.com>
2023-04-10 23:16:11 -0300

    some help tools for testing

--- 19 - Automated Tests/1 - Introduction/addresses/addresses_test.go
+++ 19 - Automated Tests/1 - Introduction/addresses/addresses_test.go
@@ -1,6 +1,17 @@
-package addresses
+package addresses_test
 
-import "testing"
+// IMPORTANT COMMANDS:
+// go test
+// go test -v  (verbose)
+// go test --cover (coverage)
+// go test --coverprofile coverage.txt
+// go tool cover --func=coverage.txt (to read the result test file)
+// go tool cover --html=coverage.txt (shows a html file with each line covered and not covered by test)
+
+import (
+	"testing"
+	. "tests-introduction/addresses" // use this notation (.) for auto function reference of this package, its kind of alias.
+)
 
 type testScenario struct {
 	insertedAddress string
@@ -8,16 +19,17 @@ type testScenario struct {
 }
 
 func TestAddressType(t *testing.T) {
+	// t.Parallel() - to run parallel tests
 
 	testScenarios := []testScenario{
 		{"Rua ABC", "Rua"},
 		{"Avenida Paulista", "Avenida"},
 		{"Rodovia dos Imigrantes", "Rodovia"},
-		{"Praça das Rosas", "Invalid Type"},
+		// {"Praça das Rosas", "Invalid Type"},
 		{"Estrada Qualquer", "Estrada"},
 		{"RUA DOS BOBOS", "Rua"},
 		{"AVENIDA REBOUÇAS", "Avenida"},
-		{"", "Invalid Type"},
+		// {"", "Invalid Type"},
 	}
 
 	for _, scenario := range testScenarios {
@@ -27,5 +39,13 @@ func TestAddressType(t *testing.T) {
 			t.Errorf("Received type %s is different than expected %s",
 				receivedReturn, scenario.expectedReturn)
 		}
+	}
+}
+
+func TestAny(t *testing.T) {
+	// t.Parallel()
+
+	if 1 > 2 {
+		t.Errorf("Broke test!")
 	}
 }
