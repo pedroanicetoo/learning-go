6a1c65907e25140164672c9e7bf6e4b0cbfe4b3e
pedroanicetoo <pedro0077@gmail.com>
2023-04-10 22:53:09 -0300

    real go test example

--- 19 - Automated Tests/1 - Introduction/addresses/addresses_test.go
+++ 19 - Automated Tests/1 - Introduction/addresses/addresses_test.go
@@ -2,16 +2,30 @@ package addresses
 
 import "testing"
 
+type testScenario struct {
+	insertedAddress string
+	expectedReturn  string
+}
+
 func TestAddressType(t *testing.T) {
-	addressToTest := "Rua ABC"
 
-	expectedAdressType := "Rua"
+	testScenarios := []testScenario{
+		{"Rua ABC", "Rua"},
+		{"Avenida Paulista", "Avenida"},
+		{"Rodovia dos Imigrantes", "Rodovia"},
+		{"Praça das Rosas", "Invalid Type"},
+		{"Estrada Qualquer", "Estrada"},
+		{"RUA DOS BOBOS", "Rua"},
+		{"AVENIDA REBOUÇAS", "Avenida"},
+		{"", "Invalid Type"},
+	}
 
-	receivedAddressType := AddressType(addressToTest)
+	for _, scenario := range testScenarios {
+		receivedReturn := AddressType(scenario.insertedAddress)
 
-	if receivedAddressType != expectedAdressType {
-		t.Errorf("Received type is different than expected! Expected %s and received %s",
-			expectedAdressType,
-			receivedAddressType)
+		if receivedReturn != scenario.expectedReturn {
+			t.Errorf("Received type %s is different than expected %s",
+				receivedReturn, scenario.expectedReturn)
+		}
 	}
 }
